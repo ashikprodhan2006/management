b40ab1daa654f0e81377fc573e6506523d56d17d
ashikprodhan2006 <ashikprodhan2006@gmail.com>
2025-02-23 20:43:32 +0600

    kino chence korinai

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,12 +1,13 @@
 
 from django.urls import path
-from tasks.views import manager_dashboard, employee_dashboard, view_task, update_task, delete_task, event_management, dashboard, event, event_form
+from tasks.views import manager_dashboard, employee_dashboard, create_task, view_task, update_task, delete_task, event_management, dashboard, event, event_form
 
 
 urlpatterns = [
     path('manager-dashboard/', manager_dashboard, name = 'manager-dashboard'),
     path('dashboard/', dashboard, name = 'dashboard'),
     path("user-dashboard/", employee_dashboard, name='user-dashboard'),
+    path('create-task/', create_task, name='create-task'),
     path('view-task/', view_task),
     path('update-task/<int:id>/', update_task, name='update-task'),
     path('delete-task/<int:id>/', delete_task, name = 'delete-task'),
