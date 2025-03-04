6d09289013dbc66df9d5bf01d5222ef9819b14ea
ashikprodhan2006 <ashikprodhan2006@gmail.com>
2025-03-05 01:34:35 +0600

    Rady for deployment

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,26 +1,4 @@
 from django.urls import path
-<<<<<<< HEAD
-from tasks.views import manager_dashboard, employee_dashboard, create_task, view_task, update_task, delete_task, event_management, dashboards, event, event_form, task_details
-
-
-urlpatterns = [
-    path('manager-dashboard/', manager_dashboard, name = 'manager-dashboard'),
-    path('dashboard/', dashboards, name = 'dashboard'),
-    path("user-dashboard/", employee_dashboard, name='user-dashboard'),
-    path('create-task/', create_task, name='create-task'),
-    path('view-task/', view_task),
-    path('update-task/<int:id>/', update_task, name='update-task'),
-    path('delete-task/<int:id>/', delete_task, name = 'delete-task'),
-    path('event-management/', event_management, name = 'event-management'),
-    path('event/', event, name = 'event'),
-    path('event-form/', event_form, name = 'event-form'),
-    path('task/<int:task_id>/details/', task_details, name='task-details'),
-    
-]
-
-
-
-=======
 from tasks.views import manager_dashboard, employee_dashboard, create_task, view_task, update_task, delete_task, task_details, dashboard, event_management, event, event_form, Greetings, HiGreetings, HiHowGreetings, CreateTask, ViewProject, TaskDetail, UpdateTask, ManagerDashboardView, EmployeeDashboardView, DeleteTaskView, EventManagementView
 
 urlpatterns = [
@@ -49,4 +27,3 @@ urlpatterns = [
     path('higreetings/', HiGreetings.as_view(), name='higreetings'),
     path('hihowgreetings/', HiHowGreetings.as_view(greetings='Hi Good Day'), name='hihowgreetings'),
 ]
->>>>>>> module-18
